1ed312cb927613707af458c7bc133379c809bc2e
Edwin <edwincastaneda0801@gmail.com>
2024-10-11 16:00:42 -0500

    post, delete, update employee added

--- api/employees.js
+++ api/employees.js
@@ -20,8 +20,71 @@ router.get("/:id", async (req, res, next) => {
     if (employee) {
       res.json(employee);
     } else {
-      next({ status: 404, message: `Employee with ${id} not found` });
+      next({ status: 404, message: `Employee with id: ${id} not found` });
     }
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return next({
+      status: 404,
+      message: "A name must be provided",
+    });
+  }
+  try {
+    const employee = await prisma.employee.create({ data: { name } });
+    res.status(201).json(employee);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const employee = await prisma.employee.findUnique({ where: { id: +id } });
+    if (!employee) {
+      return next({
+        status: 404,
+        message: `Employee with id: ${id} not found`,
+      });
+    }
+    await prisma.employee.delete({ where: { id: +id } });
+    res.sendStatus(204);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  if (!name) {
+    return {
+      status: 400,
+      message: "You must provide a new employee name",
+    };
+  }
+
+  try {
+    const employee = await prisma.employee.findUnique({ where: { id: +id } });
+    if (!employee) {
+      return next({
+        status: 404,
+        message: `Employee with id: ${id} does not exist.`,
+      });
+    }
+    const updatedEmployee = await prisma.employee.update({
+      where: { id: +id },
+      data: { name },
+    });
+    res.json(updatedEmployee);
   } catch (e) {
     next(e);
   }
